c9ef10a6e74e39ef6dcc402382657e719f34fb87
Marcooos05 <108853663+Marcooos05@users.noreply.github.com>
2024-01-04 10:59:04 +0000

    [LeetCode Sync] Runtime - 34 ms (6.74%), Memory - 13.6 MB (0.00%)

--- submissions/2724-convert-an-array-into-a-2d-array-with-conditions/solution.py
+++ submissions/2724-convert-an-array-into-a-2d-array-with-conditions/solution.py
@@ -4,16 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        counts = []
         output = []
+        count = {}
+        for val in nums:
+            if val not in count:
+                if len(output) == 0:
+                    output.append([])
+                output[0].append(val)
+                count[val] = 1
+            else:
+                count[val] += 1
+                if count[val] > len(output):
+                    output.append([])
+                output[count[val]-1].append(val)
+                
+        return output
 
+    def second_ans(self,nums):
         unique_nums = set(nums)
         for val in unique_nums:
             num_count = nums.count(val)
             for idx in range(num_count):
-                try:
+                if len(output) > idx:
                     output[idx].append(val)
-                except:
+                else:
                     output.append([val])
 
         return output
