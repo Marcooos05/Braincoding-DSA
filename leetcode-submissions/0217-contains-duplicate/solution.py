bf2da79719694210b958b6629623d43c868af825
Marcooos05 <108853663+Marcooos05@users.noreply.github.com>
2026-08-21 02:38:46 +0000

    [LeetCode Sync] Runtime - 2 ms (98.21%), Memory - 32.2 MB (52.90%)

--- leetcode-submissions/0217-contains-duplicate/solution.py
+++ leetcode-submissions/0217-contains-duplicate/solution.py
@@ -1,14 +1,24 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        dictionary = {}
-        for i in nums:
-            if i not in dictionary:
-                dictionary[i] = 1
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
+
+    def containsDuplicate_hashAttempt(self, nums: List[int]) -> bool:
+        #using hash to possibly improve memory usage
+        hashCount = {}
+        for num in nums:
+            if num not in hashCount:
+                hashCount[num] = 1
             else:
                 return True
         return False
-        
+    
+    def containsDuplicate_setAttempt(self, nums: List[int]) -> bool:
+        #set difference method, good runtime, memory not optimal
+        #dup = set() - initally used a duplicate set to track duplicates but not actually needed, it can be simplified to return immediately on detecting a duplicate
+        seen = set()
+        for num in nums:
+            if num not in seen:
+                seen.add(num)
+            else:
+                return True
+        return False
